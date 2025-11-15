afaa6872ad6f91326fac80d408d3df3bdaa08f99
Freddy Francis <freddyfrancis3102@gmail.com>
2025-11-15 12:10:23 +0530

    first clean commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch("https://docker-mini-backend-imo5.onrender.com/api/hello") 
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <h1>Frontend + Backend + DB with Docker</h1>
+      <p>Message from backend: <b>{message}</b></p>
+    </div>
+  );
+}
+
+export default App;
+
